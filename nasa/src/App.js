f05078113e5b677b1f8df2b753aa10b91588307f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-17 15:48:27 -0500

    deleted react files and made columns for css

---
+++ nasa/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Nasa from "../src/Nasa"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nasa />
+    </div>
+  );
+}
+
+export default App;
