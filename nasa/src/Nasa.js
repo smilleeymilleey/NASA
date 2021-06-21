4d8d930827c33f2f6752d7b5ad885cb338ac54a8
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-20 20:02:20 -0500

    added more styling to the page

--- nasa/src/Nasa.js
+++ nasa/src/Nasa.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import "../src/Nasa.css"
 
 function Nasa() {
     return (
         <div class="container">
         <div class="row">
-            <div class="col-sm-5 col-md-6">Cool Nasa Data Title</div>
-            <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">API INFO HERE</div>
+            <div class="nasaTitle col-sm-5 col-md-6">Nasa Data Title </div>
+            <div class="nasaDataOne col-sm-5 offset-sm-2 col-md-6 offset-md-0">API INFO HERE</div>
         </div>
         <div class="row">
-            <div class="col-sm-6 col-md-5 col-lg-6">More Nasa Data Title</div>
-            <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">API INFO HERE</div>
+            <div class="nasaDataTwo col-sm-6 col-md-5 col-lg-6">More Nasa Data Title</div>
+            <div class="nasaDataThree col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">API INFO HERE</div>
         </div>
         </div>
     )
