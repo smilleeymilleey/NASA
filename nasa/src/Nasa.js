5d8f2d66a800fc8d605eebe4b5905d87f2dd3b69
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-21 12:38:02 -0500

    added cards to better UI

--- nasa/src/Nasa.js
+++ nasa/src/Nasa.js
@@ -3,16 +3,27 @@ import "../src/Nasa.css"
 
 function Nasa() {
     return (
-        <div class="container">
         <div class="row">
-            <div class="nasaTitle col-sm-5 col-md-6">Nasa Data Title </div>
-            <div class="nasaDataOne col-sm-5 offset-sm-2 col-md-6 offset-md-0">API INFO HERE</div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
         </div>
-        <div class="row">
-            <div class="nasaDataTwo col-sm-6 col-md-5 col-lg-6">More Nasa Data Title</div>
-            <div class="nasaDataThree col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">API INFO HERE</div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
+      
     )
 }
 
